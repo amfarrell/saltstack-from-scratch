d37fd4a84e853901f4a48c23770c43fcd48755de
Andrew M. Farrell <amfarrell@mit.edu>
2015-11-14 18:55:54 -0600

    Add ag.sls and install-salt.sh

---
+++ samwise_vagrant/install-salt.sh
@@ -0,0 +1,6 @@
+add-apt-repository ppa:saltstack/salt -y
+apt-get update -y
+apt-get install salt-minion -y
+cp /etc/salt/minion /etc/salt/minion.bak
+cat /etc/salt/minion.bak | sed 's/^#log_level_logfile:/log_level_logfile: info/' | sudo sed 's/^#master:\ salt/master: frodo/' > /etc/salt/minion
+service salt-minion restart
