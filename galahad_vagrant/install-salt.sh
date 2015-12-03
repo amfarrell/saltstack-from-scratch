06d4b3278639321440008ec1791db73f7beaeac6
Andrew M. Farrell <amfarrell@mit.edu>
2015-12-03 14:40:17 -0600

    replace install.sh with the master and minion config files.

--- galahad_vagrant/install-salt.sh
+++
@@ -1,6 +0,0 @@
-add-apt-repository ppa:saltstack/salt -y
-apt-get update -y
-apt-get install salt-minion -y
-cp /etc/salt/minion /etc/salt/minion.bak
-cat /etc/salt/minion.bak | sed 's/^#log_level_logfile:/log_level_logfile: info/' | sudo sed 's/^#master:\ salt/master: arthur/' > /etc/salt/minion
-service salt-minion restart
