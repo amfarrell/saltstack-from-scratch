2170e6a26473b904de1db93c57f8d50696d30445
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-12 17:57:28 -0600

    Move tests to a file named based on purpose.

---
+++ tests/test_vagrant.py
@@ -0,0 +1,34 @@
+import subprocess
+import pytest
+from test_utils import run
+
+setup = pytest.mark.setup
+complete = pytest.mark.complete
+
+@setup
+def test_virtualbox_version():
+    """
+    Shell out to vboxmanage to get the version of virtualbox currently installed.
+
+    TODO: Check to see if this works on windows.
+    TODO: Make this less brittle with respect to versions of virtualbox.
+    """
+    correct_version = '5.0.6'
+    try:
+        version = run(['vboxmanage','--version'])
+    except FileNotFoundError:
+        assert False, "Virtualbox is not yet installed."
+    assert correct_version in version, "Your version of virtualbox is {}.\n \
+        This may not work with the rest of the tutorial,\n \
+        which is written for Virtualbox {}.".format(version, correct_version)
+
+@setup
+def test_vagrant_version():
+    correct_version = '1.7.4'
+    try:
+        version = run(['vagrant','--version'])
+    except FileNotFoundError:
+        assert False, "Vagrant is not yet installed."
+    assert correct_version in version, "Your version of vagrant is {}.\n \
+        This may not work with the rest of the tutorial,\n \
+        which is written for vagrant {}.".format(version, correct_version)
