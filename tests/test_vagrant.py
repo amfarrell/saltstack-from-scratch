91dbfddc0085dfb191197d32fd068040b43da9ec
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-13 16:55:24 -0600

    split tests into setup and complete.

--- tests/test_vagrant.py
+++ tests/test_vagrant.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
 import re
-from test_utils import check_tests_run_from_base_dir, run, run_galahad, run_arthur
+from test_utils import check_tests_run_from_base_dir, \
+    run, run_galahad, run_arthur, setup, complete
 
+@setup
 def test_virtualbox_version():
     """
     Shell out to vboxmanage to get the version of virtualbox currently installed.
@@ -19,6 +21,7 @@ def test_virtualbox_version():
         This may not work with the rest of the tutorial,\n \
         which is written for Virtualbox {}.".format(version, correct_version)
 
+@setup
 def test_vagrant_version():
     correct_version = '1.7.4'
     try:
@@ -29,26 +32,35 @@ def test_vagrant_version():
         This may not work with the rest of the tutorial,\n \
         which is written for vagrant {}.".format(version, correct_version)
 
+@setup
 def test_vagrantfile_exists():
     assert os.path.exists('Vagrantfile'), \
         "You must create a vagrantfile at {}".format(os.path.abspath('Vagrantfile'))
 
+@complete
 def test_single_box_not_running():
     global_status = run(['vagrant', 'global-status'])
     assert 'default virtualbox' not in global_status, "the single box created in step 1 is still runnning. remove it with the `vagrant destroy` command."
 
+@complete
 def test_double_boxes_running():
     check_tests_run_from_base_dir()
     global_status = run(['vagrant', 'global-status'])
-    assert "sir-galahad virtualbox running {}".format(os.getcwd()) in global_status
+    assert "galahad virtualbox running {}".format(os.getcwd()) in global_status
     assert "arthur  virtualbox running {}".format(os.getcwd()) in global_status
     assert os.path.exists('arthur_vagrant')
     assert os.path.exists('galahad_vagrant')
 
+@complete
 def test_hostmanager_installed():
     plugins = run(['vagrant', 'plugin', 'list'])
     assert 'vagrant-hostmanager' in plugins
 
-def test_samwise_can_ping_frodo():
-    assert 'unknown host' not in run_galahad('ping -c 1 arthur'), \
-        "arthur cannot ping galahad"
+@complete
+def test_galahad_can_ping_arthur():
+    ping_results = run_galahad('ping -c 1 arthur')
+    assert 'unknown host' not in ping_results, \
+        "galahad does not know arthur's IP address"
+    assert 'Unreachable' not in ping_results, \
+        "arthur is not responding to ping"
+    assert '1 packets transmitted' in ping_results
