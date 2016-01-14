1ed2eaab3f5c6ba10efd77db83bb6a95c7ac20cc
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-13 21:09:42 -0600

    Split tests into different files.

---
+++ tests/test_states.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+import re
+from test_utils import run, run_galahad, run_arthur, setup, complete
+
+@complete
+def test_ag_installed_on_galahad():
+    assert 'ok installed' in run_galahad('dpkg -s silversearcher-ag | grep Status'), \
+        "You must ensure that silversearcher-ag is installed on galahad."
+
+@complete
+def test_ag_installed_on_arthur():
+    assert 'ok installed' in run_arthur('dpkg -s silversearcher-ag | grep Status'), \
+        "You must ensure that silversearcher-ag is installed on arthur."
