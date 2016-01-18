be8c6b303ac68584e54e5b82b387e3d7133d48c5
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-18 02:20:07 -0600

    split tests into multiple files.

--- tests/test_states_django.py
+++ tests/test_states_django.py
@@ -1,9 +1,15 @@
-from test_utils import run_arthur, run_galahad, SaltStateTestCase
-import shutil
-import os.path
-import unittest
-import yaml
+from __future__ import absolute_import
 
+import os.path
+import shutil
+import subprocess
+import unittest
+import re
+import yaml
+from tests.test_utils import run, run_galahad, run_arthur, \
+    SaltStateTestCase, setup, complete
+
+@setup
 class TestGalahadDjangoExample(SaltStateTestCase):
 
     def debian_package_installed(self, packagename):
@@ -15,7 +21,7 @@ class TestGalahadDjangoExample(SaltStateTestCase):
             run_galahad('sudo apt-get remove python-virtualenv')
         if os.path.exists('galahad_vagrant/example-venv/bin/'):
             run_galahad('rm -rf /vagrant/example-venv')
-            #remove directory from guest because 
+            #remove directory from guest because
             #removing from host triggers a virtualbox bug
         state_response_data = self.run_state(state_file='django', target='galahad')
         assert self.debian_package_installed('python-virtualenv')
@@ -26,7 +32,7 @@ class TestGalahadDjangoExample(SaltStateTestCase):
     def test_git_repo_exists(self):
         if os.path.exists('galahad_vagrant/django-example'):
             run_galahad('rm -rf /vagrant/django-example')
-            #remove directory from guest because 
+            #remove directory from guest because
             #removing from host triggers a virtualbox bug
         if self.debian_package_installed('git'):
             run_galahad('sudo apt-get remove git')
