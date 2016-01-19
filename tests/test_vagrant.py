212e22437484768abe2ba5ac00beb4ecef2fdde9
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-19 11:49:53 -0600

    compare vagrant and virtualbox versions more robustly.

--- tests/test_vagrant.py
+++ tests/test_vagrant.py
@@ -5,6 +5,7 @@ import os
 import re
 from tests.test_utils import check_tests_run_from_base_dir, \
     run, run_galahad, run_arthur, setup, complete
+from distutils.version import LooseVersion
 
 @setup
 def test_start():
@@ -21,20 +22,20 @@ def test_virtualbox_version():
     TODO: Check to see if this works on windows.
     TODO: Make this less brittle with respect to versions of virtualbox.
     """
-    correct_version = '5.0.6'
+    correct_version = LooseVersion('5.0.6')
     try:
-        version = run(['vboxmanage','--version'])
+        version = LooseVersion(run(['vboxmanage','--version']))
     except FileNotFoundError:
         assert False, "Virtualbox is not yet installed."
-    assert correct_version in version, "Your version of virtualbox is {}.\n \
+    assert correct_version <= version, "Your version of virtualbox is {}.\n \
         This may not work with the rest of the tutorial,\n \
         which is written for Virtualbox {}.".format(version, correct_version)
 
 @setup
 def test_vagrant_version():
-    correct_version = '1.7.4'
+    correct_version = LooseVersion('1.7.4')
     try:
-        version = run(['vagrant','--version'])
+        version = LooseVersion(run(['vagrant','--version']))
     except FileNotFoundError:
         assert False, "Vagrant is not yet installed."
     assert correct_version in version, "Your version of vagrant is {}.\n \
